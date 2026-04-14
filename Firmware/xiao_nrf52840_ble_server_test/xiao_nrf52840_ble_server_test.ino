73adb939502d515357be0ebf62085dd3f1fb467d
piotrszlejter <petequerrer@gmail.com>
2026-04-14 12:55:16 +0200

    added test xiao nrf522840 .ino, added .gitignore

---
+++ Firmware/xiao_nrf52840_ble_server_test/xiao_nrf52840_ble_server_test.ino
@@ -0,0 +1,70 @@
+#include <bluefruit.h>
+
+// Custom UUIDs for the Service and Characteristic
+BLEUuid svc_uuid = BLEUuid("4fafc201-1fb5-459e-8fcc-c5c9c331914b");
+BLEUuid chr_uuid = BLEUuid("beb5483e-36e1-4688-b7f5-ea07361b26a8");
+
+BLEService gloveService(svc_uuid);
+BLECharacteristic gloveCharacteristics(chr_uuid);
+
+void setup() {
+  Serial.begin(115200);
+
+  // Initialize the Bluefruit module
+  Bluefruit.begin();
+  Bluefruit.setName("XIAO_Sensor");
+
+  // Setup the Service
+  gloveService.begin();
+
+  // Setup the Characteristic with Read and Notify properties
+  gloveCharacteristics.setProperties(CHR_PROPS_READ | CHR_PROPS_NOTIFY);
+  gloveCharacteristics.setPermission(SECMODE_OPEN, SECMODE_NO_ACCESS);
+  gloveCharacteristics.setMaxLen(100);
+  gloveCharacteristics.begin();
+
+  gloveCharacteristics.setUserDescriptor("glove data");
+
+  // Set an initial starting value
+  gloveCharacteristics.write("A100B100C100D100E100");
+
+
+
+  // Configure and start advertising
+  Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
+  Bluefruit.Advertising.addTxPower();
+  Bluefruit.Advertising.addService(gloveService);
+  Bluefruit.Advertising.addName();
+  Bluefruit.Advertising.restartOnDisconnect(true);
+  
+  // 0 means it will advertise forever without timing out
+  Bluefruit.Advertising.start(0);
+  
+  Serial.println("Waiting for a client connection to notify...");
+}
+
+void loop() {
+  // Check if a device is currently connected
+  if (Bluefruit.connected()) {
+    
+    // Generate a random temperature between 20 and 30
+    int randomTemp = random(20, 31);
+    int randIndexf = random (1000, 5500);
+    int randMiddlef = random (1000, 5500);
+    int randRingf = random (1000, 5500);
+    int randPinkyf = random (1000, 5500);
+    int randThumbf = random (1000, 5500);
+    //String tempVal = String(randomTemp) + " C";
+
+    String sendVal = "A" + String(randIndexf) + "B" + String(randMiddlef)
+    + "C" + String(randRingf) + "D" + String(randPinkyf) + "E" + String(randThumbf);
+    // Push the notification to the connected client
+    gloveCharacteristics.notify(sendVal.c_str(), sendVal.length());
+
+    Serial.print("Notified value: ");
+    Serial.println(sendVal);
+
+    // Wait one second before sending the next value
+    delay(5000);
+  }
+}
