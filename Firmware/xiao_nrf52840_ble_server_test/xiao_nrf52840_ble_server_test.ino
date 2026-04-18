9cdb81f5968cbb3c3cb53a1ccd273f6c898439dc
piotrszlejter <petequerrer@gmail.com>
2026-04-18 02:37:01 +0200

    fixed: fixed broken xiao .ino code

--- Firmware/xiao_nrf52840_ble_server_test/xiao_nrf52840_ble_server_test.ino
+++ Firmware/xiao_nrf52840_ble_server_test/xiao_nrf52840_ble_server_test.ino
@@ -1,47 +1,31 @@
 #include <bluefruit.h>
 
-//#define PIN_VBAT A6
-//#define PIN_VBAT_ENABLE 14
-
-
-
+// Custom UUIDs for the Service and Characteristic
 BLEUuid svc_uuid = BLEUuid("4fafc201-1fb5-459e-8fcc-c5c9c331914b");
 BLEUuid chr_uuid = BLEUuid("beb5483e-36e1-4688-b7f5-ea07361b26a8");
 
 BLEService gloveService(svc_uuid);
 BLECharacteristic gloveCharacteristics(chr_uuid);
 
-//BLEBas batteryService;
-
 char sendBuffer[64];
 
-//uint32_t timer_battery_update = 0;
-//int debug_adc = 0;
 
 void setup() {
   Serial.begin(115200);
 
+  Bluefruit.configPrphConn(64, BLE_GAP_EVENT_LENGTH_DEFAULT, BLE_GATTS_HVN_TX_QUEUE_SIZE_DEFAULT, BLE_GATTC_WRITE_CMD_TX_QUEUE_SIZE_DEFAULT);
+
+
+  // Initialize the Bluefruit module
   Bluefruit.begin();
   Bluefruit.setName("XIAO_Sensor");
 
   Bluefruit.Periph.setConnInterval(6,12);
 
-  //batteryService.begin();
-  //batteryService.write(100);
-
-  pinMode(VBAT_ENABLE, OUTPUT);
-  digitalWrite(VBAT_ENABLE, HIGH);
-
-  Bluefruit.configPrphConn(64, BLE_GAP_EVENT_LENGTH_DEFAULT, BLE_GATTS_HVN_TX_QUEUE_SIZE_DEFAULT, BLE_GATTC_WRITE_CMD_TX_QUEUE_SIZE_DEFAULT);
-
-
-
-
-
-
+  // Setup the Service
   gloveService.begin();
 
-
+  // Setup the Characteristic with Read and Notify properties
   gloveCharacteristics.setProperties(CHR_PROPS_READ | CHR_PROPS_NOTIFY);
   gloveCharacteristics.setPermission(SECMODE_OPEN, SECMODE_NO_ACCESS);
   gloveCharacteristics.setMaxLen(10);
@@ -49,27 +33,26 @@ void setup() {
 
   gloveCharacteristics.setUserDescriptor("glove data");
 
+  // Set an initial starting value
+  gloveCharacteristics.write("A100B100C100D100E100");
 
- // gloveCharacteristics.write("A100B100C100D100E100");
 
 
+  // Configure and start advertising
   Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
   Bluefruit.Advertising.addTxPower();
-
   Bluefruit.Advertising.addService(gloveService);
-  //Bluefruit.Advertising.addService(batteryService);
-
   Bluefruit.Advertising.addName();
   Bluefruit.Advertising.restartOnDisconnect(true);
   
-
+  // 0 means it will advertise forever without timing out
   Bluefruit.Advertising.start(0);
   
   Serial.println("Waiting for a client connection to notify...");
 }
 
 void loop() {
-
+  // Check if a device is currently connected
   if (Bluefruit.connected()) {
 
     int16_t fingers[5];
@@ -80,13 +63,30 @@ void loop() {
     fingers[3] = random(1000, 5500);
     fingers[4] = random(1000, 5500);
     
-   
-    
-
-    //uint16_t raw_val = debug_adc;
     gloveCharacteristics.notify(fingers, sizeof(fingers));
 
-    //timer_battery_update += 1;
-    delay(10);
+    // Generate a random temperature between 20 and 30
+    /*
+    int randomTemp = random(20, 31);
+    int randIndexf = random (1000, 5500);
+    int randMiddlef = random (1000, 5500);
+    int randRingf = random (1000, 5500);
+    int randPinkyf = random (1000, 5500);
+    int randThumbf = random (1000, 5500);
+    //String tempVal = String(randomTemp) + " C";
+    
+    //String sendVal = "A" + String(randIndexf) + "B" + String(randMiddlef)
+    //+ "C" + String(randRingf) + "D" + String(randPinkyf) + "E" + String(randThumbf);
+    snprintf(sendBuffer, sizeof(sendBuffer), "A%dB%dC%dD%dE%d", randIndexf, 
+    randMiddlef, randRingf, randPinkyf, randThumbf);
+    
+    // Push the notification to the connected client
+    gloveCharacteristics.notify(sendBuffer, strlen(sendBuffer));
+    */
+    //Serial.print("Notified value: ");
+    //Serial.println(sendBuffer);
+
+    // Wait one second before sending the next value
+    //delay(5);
   }
 }
